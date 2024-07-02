e84ab2cbc8b4ece9a6b5b6172e3668c78177ad14
zhangyu1 <15621086368@163.com>
2024-07-02 09:11:57 +0800

    新增：加入@RequestMapping、@Tag和@Operation规范代码

--- cloud-consumer-order80/src/main/java/com/wasai/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/wasai/controller/OrderController.java
@@ -2,7 +2,12 @@ package com.wasai.controller;
 
 import com.wasai.entities.PayDTO;
 import com.wasai.resp.ResultData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,7 +17,10 @@ import org.springframework.web.client.RestTemplate;
  * @time 18:11
  * @desc
  */
+@Slf4j
+@Tag(name = "订单服务")
 @RestController
+@RequestMapping("/consumer")
 public class OrderController {
 //    public static final String PaymentSrv_URL = "http://localhost:8001";//先写死，硬编码
     public static final String PaymentSrv_URL = "http://cloud-payment-service";//服务注册中心上的微服务名称
@@ -21,33 +29,39 @@ public class OrderController {
     private RestTemplate restTemplate;
 
     //新增
-    @GetMapping(value = "/consumer/pay/add")
+    @Operation(summary = "新增", parameters = {
+            @Parameter(in = ParameterIn.DEFAULT, name = "payNo", description = "支付编号", required = true, example = "XX"),
+            @Parameter(in = ParameterIn.DEFAULT, name = "orderNo", description = "订单编号", required = true, example = "XX"),
+            @Parameter(in = ParameterIn.DEFAULT, name = "userId", description = "用户编号", required = true, example = "XX"),
+            @Parameter(in = ParameterIn.DEFAULT, name = "amount", description= "金额", required = true, example = "XX")
+    })
+    @RequestMapping(value = "/pay/add",method = RequestMethod.GET)
     public ResultData addOrder(PayDTO payDTO) {
         return restTemplate.postForObject(PaymentSrv_URL + "/pay/add", payDTO, ResultData.class);
     }
 
     //查询
-    @GetMapping(value = "/consumer/pay/get/{id}")
+    @GetMapping(value = "/pay/get/{id}")
     public ResultData getPayInfo(@PathVariable("id") Integer id) {
         return restTemplate.getForObject(PaymentSrv_URL + "/pay/get/" + id, ResultData.class);
     }
 
     //删除
-    @GetMapping(value = "/consumer/pay/delete/{id}")
+    @GetMapping(value = "/pay/delete/{id}")
     public ResultData deletePayInfo(@PathVariable("id") Integer id) {
         restTemplate.delete(PaymentSrv_URL + "/pay/delete/" + id);
         return ResultData.success("删除成功");
     }
 
     //修改
-    @GetMapping(value = "/consumer/pay/update")
+    @GetMapping(value = "/pay/update")
     public ResultData updatePayInfo(PayDTO payDTO) {
         restTemplate.put(PaymentSrv_URL + "/pay/update", payDTO);
         return ResultData.success("修改成功");
     }
 
     //获取全部订单
-    @GetMapping(value = "/consumer/pay/getAll")
+    @GetMapping(value = "/pay/getAll")
     public ResultData getAllPay() {
         return restTemplate.getForObject(PaymentSrv_URL + "/pay/getAll", ResultData.class);
     }
